339946d2d5d337901ce66600e167e6efd5928dcf
Stanislav Dumchykov <mechanikea@gmail.com>
2024-08-09 21:26:44 +0300

    Corrected status bar color

--- app/src/main/java/com/dumchykov/socialnetworkdemo/MainActivity.kt
+++ app/src/main/java/com/dumchykov/socialnetworkdemo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.dumchykov.socialnetworkdemo
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
@@ -24,7 +26,9 @@ import com.dumchykov.socialnetworkdemo.ui.theme.SocialNetworkClientTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
         setContent {
             SocialNetworkClientTheme {
                 Scaffold(
