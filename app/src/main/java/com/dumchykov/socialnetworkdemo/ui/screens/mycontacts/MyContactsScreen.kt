82f79c7e67516e04c5b530b3f3cf9eed51ac831b
Stanislav Dumchykov <mechanikea@gmail.com>
2024-08-12 15:49:45 +0300

    Handled landscape orientation.

--- app/src/main/java/com/dumchykov/socialnetworkdemo/ui/screens/mycontacts/MyContactsScreen.kt
+++ app/src/main/java/com/dumchykov/socialnetworkdemo/ui/screens/mycontacts/MyContactsScreen.kt
@@ -1,5 +1,7 @@
 package com.dumchykov.socialnetworkdemo.ui.screens.mycontacts
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
@@ -64,6 +66,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -98,6 +101,9 @@ fun MyContactsScreen(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
+    val context = LocalContext.current
+    (context as? Activity)?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
     val viewModel: MyContactsViewModel = viewModel(factory = MyContactsViewModel.factory())
     val myContactsState = viewModel.myContactsState.collectAsState().value
     val scope = rememberCoroutineScope()
