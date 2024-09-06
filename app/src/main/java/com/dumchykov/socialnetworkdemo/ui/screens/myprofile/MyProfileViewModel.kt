2981b349d56306841463e5beede5b5d104a46745
Stanislav Dumchykov <mechanikea@gmail.com>
2024-09-06 09:57:50 +0300

    Updated MyProfileViewModel.

--- app/src/main/java/com/dumchykov/socialnetworkdemo/ui/screens/myprofile/MyProfileViewModel.kt
+++ app/src/main/java/com/dumchykov/socialnetworkdemo/ui/screens/myprofile/MyProfileViewModel.kt
@@ -2,42 +2,41 @@ package com.dumchykov.socialnetworkdemo.ui.screens.myprofile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dumchykov.database.ContactsDatabase
 import com.dumchykov.datastore.data.DataStoreProvider
 import com.dumchykov.socialnetworkdemo.ui.screens.myprofile.models.toMyProfileContact
-import com.dumchykov.socialnetworkdemo.webapi.domain.ContactRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class MyProfileViewModel @Inject constructor(
-    private val contactRepository: ContactRepository,
+    private val database: ContactsDatabase,
     private val dataStoreProvider: DataStoreProvider,
 ) : ViewModel() {
     private val _myProfileState = MutableStateFlow(MyProfileState())
     val myProfileState get() = _myProfileState.asStateFlow()
 
     init {
+        readAuthorizedUser()
+    }
+
+    private fun readAuthorizedUser() {
         viewModelScope.launch {
-            val user = contactRepository.getCurrentUser().toMyProfileContact()
-            update { copy(user = user) }
+            val user = withContext(Dispatchers.IO) {
+                database.contactDao.getCurrentUser()
+            }.toMyProfileContact()
+            updateState { copy(user = user) }
         }
     }
 
-    fun update(reducer: MyProfileState.() -> MyProfileState) {
+    private fun updateState(reducer: MyProfileState.() -> MyProfileState) {
         _myProfileState.update(reducer)
-    }
-
-    private fun parseEmail(email: String): String {
-        return email
-            .substringBefore('@')
-            .split('.')
-            .fold("") { acc, s -> "$acc ${s.replaceFirstChar { it.uppercase() }} " }
-            .trim()
-
     }
 
     fun clearCredentials() {
